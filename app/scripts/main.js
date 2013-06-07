15566dba61f1efb560b99f25948a9c20809f3c34
Ryan Benson <benson.ryan@gmail.com>
2013-06-07 10:17:34 -0500

    Remove unneeded files and includes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,6 @@
 require.config({
   paths: {
     'jquery': 'vendor/jquery/jquery',
-    'underscore': 'vendor/underscore-amd/underscore',
-    'backbone': 'vendor/backbone-amd/backbone',
   }
 });
 
