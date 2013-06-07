3fcab74cf6c35ea2358a0ddd50e935927ba649c8
Ryan Benson <benson.ryan@gmail.com>
2013-06-07 10:30:44 -0500

    Set the basic config, may refactor later

---
+++ app/scripts/api/config/config.php
@@ -0,0 +1,9 @@
+<?php
+class Config {
+    public $appName = 'QAIntegration';
+    public $appContact = 'ryan.benson@digitalthrive.com';
+    public $basecampAccountId = '1786889';
+    public $basecampUsername = 'ryan.benson@warrendouglas.com';
+    public $basecampPassword = 'solidsnake';
+}
+?>
