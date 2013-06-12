e2b7bc8555109de731ff36f8e9bd885d833f777a
Ryan Benson <benson.ryan@gmail.com>
2013-06-12 12:10:24 -0500

    Move files around, include some front-end vendors

---
+++ app/api/classes/App.php
@@ -0,0 +1,64 @@
+<?php
+class App {
+    //our holders for configs
+    public $Config;
+    public $Debugger;
+    //app variables
+    public $httpMode;
+
+    //let's start this party
+    public function __construct()
+    {
+        //get our database and core configs and startup & hold onto the objects
+        require_once('api/config/Config.php');
+        require_once('api/classes/Debugger.php');
+        require_once('api/vendor/basecamp/basecamp.php');
+
+        //grab hold of our configs
+        $this->Config = new Config;
+        $this->Debugger = new Debugger;
+
+        //enable debug mode if necessary
+        if($this->Config->debug === true) {
+            $this->Debugger->enableDebugMode();
+        }
+        echo 'fjsdla';
+        //start the app
+        $this->start();
+    }
+
+    public function start()
+    {
+        //get the request mode
+        $this->httpMode = $_SERVER['REQUEST_METHOD'];
+        echo $this->httpMode;
+
+        //setup the basecamp api
+        $basecamp = basecamp_api_client($this->Config->appName, $this->Config->appContact,
+            $this->Config->basecampAccountId, $this->Config->basecampUsername, $this->Config->basecampPassword);
+
+        //try what we need to do
+        try {
+            /**
+             * Get a list of all projects:
+             */
+            $projects = $basecamp($this->httpMode, '/' . $_SERVER['REDIRECT_QUERY_STRING']);
+            echo $projects;
+
+            /**
+             * Create a new project:
+             * $project = array(
+             *     'name' => 'My new project!',
+             * );
+             * 
+             * $newProject = $basecamp('POST', '/projects.json', $project);
+             * echo "New project ID is {$newProject->id}\n";
+             */
+
+        //catch any errors
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+    }
+}
+?>
--- app/index.php
+++ app/index.php
@@ -1,3 +1,10 @@
+<?php
+        error_reporting(E_ALL);
+        ini_set('display_errors', 'on');
+require('api/classes/App.php');
+$app = new App;
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -6,10 +13,15 @@
     <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width">
+    <link href='http://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'>
     <link href="/styles/app.css" media="all" rel="stylesheet" />
   </head>
 
   <body>
     <h1>Quality Assuance Generator</h1>
+    <h2>Select Account Executive</h2>
+    <form method="post">
+    
+    </form>
   </body>
 </html>
