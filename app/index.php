507bd8b348ac8bff4940dacfd747c773cd38afcd
Ryan Benson <benson.ryan@gmail.com>
2013-06-12 16:04:02 -0500

    Setup a basic template

--- app/index.php
+++ app/index.php
@@ -2,25 +2,11 @@
 require('api/classes/App.php');
 $app = new App;
 $accountExecs = $app->getPeople('accountexecutives');
+
+//pull in the template
+require_once('views/layouts/header.php');
 ?>
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width">
-    <link href='http://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'>
-    <link href="/styles/app.css" media="all" rel="stylesheet" />
-    <script src="/vendor/jquery/jquery.min.js"></script>
-    <script src="/scripts/app.js"></script>
-  </head>
-
-  <body>
-    <div id="container">
-      <h1>Quality Assuance Generator</h1>
       <h2>Select Account Executive</h2>
       <div id="people" class="single">
         <table border="0">
@@ -36,6 +22,8 @@ $accountExecs = $app->getPeople('accountexecutives');
           </tr>
         </table>
       </div>
-    </div>
-  </body>
-</html>
+
+<?php
+//finish the template
+require_once('views/layouts/footer.php');
+?>
---
+++ app/views/layouts/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <link href='http://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'>
+    <link href="/styles/app.css" media="all" rel="stylesheet" />
+    <script src="/vendor/jquery/jquery.min.js"></script>
+    <script src="/scripts/app.js"></script>
+  </head>
+
+  <body>
+    <div id="container">
+      <h1>Quality Assuance Generator</h1>
