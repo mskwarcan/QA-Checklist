e7683be3ef42700419c331013fbc2c76f0708714
Ryan Benson <benson.ryan@gmail.com>
2013-06-13 12:46:35 -0500

    Got the basics down, pulling BC data into a select using select2, for selecting a project

---
+++ app/project.php
@@ -0,0 +1,35 @@
+<?php
+require('api/classes/App.php');
+$app = new App;
+$allProjects = $app->run('GET', '/projects.json');
+$allProjects = json_decode($allProjects, true);
+
+
+if(isset($_POST['submit'])) {
+  if(isset($_POST['project'])) {
+    $app->Session->set('project', $_POST['project']);
+    $app->redirect('projectsize.php');
+  }
+}
+
+$app->Layout->renderHeader();
+?>
+      <link href="/vendor/select2/select2.css" rel="stylesheet"/>
+      <script src="/vendor/select2/select2.min.js"></script>
+      <script>
+      $(document).ready(function() { $("#e1").select2(); });
+      </script>
+      
+      <h2>Select Project</h2>
+      <form method="post">
+        <select id="e1">
+          <?php foreach($allProjects as $project): ?>
+          <option value="<?php echo $project['id']; ?>"><?php echo $project['name']; ?></option>
+          <?php endforeach; ?>
+        </select>
+        <input type="submit" name="submit" id="submit" value="Continue" />
+      </form>
+
+<?php
+$app->Layout->renderFooter();
+?>
