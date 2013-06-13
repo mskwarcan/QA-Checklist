dd2ce24d4f4c97abdfcaec7760aff39c234c4909
Ryan Benson <benson.ryan@gmail.com>
2013-06-13 10:29:14 -0500

    Create session handler class

---
+++ app/api/classes/SessionHander.php
@@ -0,0 +1,21 @@
+<?php
+class SessionHander {
+    
+    public function __construct()
+    {
+        session_start();
+    }
+
+    public function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+        return true;
+    }
+
+    public function get($key)
+    {
+        return $_SESSION[$key];
+    }
+
+}
+?>
